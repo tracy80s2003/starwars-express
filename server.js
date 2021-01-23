dd7651bd35c34bf5ee54be19440c8e1643e4f334
Tracy Golden <tracy80s2003@gmail.com>
2021-01-23 13:49:18 -0600

    added files and updated

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const PORT = 3000
@@ -33,7 +34,11 @@ const characters = [
 ]
 
 app.get('/', (req, res) => {
-    res.send('May the force be with you!')
+    res.sendFile( path.join(__dirname + '/public/index.html'))
+})
+
+app.get('/add', (req, res) => {
+    res.sendFile( path.join(__dirname + '/public/add.html'))
 })
 
 app.get('/api/characters', (req,res) => {
