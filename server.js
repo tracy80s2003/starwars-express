bee74c95ce67cec935708c584055d495fd7d78ef
Tracy Golden <tracy80s2003@gmail.com>
2021-01-23 11:09:40 -0600

    added /api/characters route

--- server.js
+++ server.js
@@ -2,8 +2,39 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const characters = [
+    {
+        name: 'Yoda',
+        role: 'Jedi Master', 
+        forcePoints: 100000,
+        age: 900,
+        avatarImg: 'https://static.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png/revision/latest/scale-to-width-down/500?cb=20150206140125',
+        routeName: 'yoda'
+    },
+    {
+        name: 'Luke Skywalker', 
+        role: 'Jedi Master', 
+        forcePoints: 10000,
+        age: 40,
+        avatarImg: 'https://media.contentapi.ea.com/content/dam/star-wars-battlefront-2/images/2019/08/swbf2-refresh-hero-large-heroes-page-luke-skywalker-16x9-xl.jpg.adapt.crop1x1.320w.jpg',
+        routeName: 'lukeskywalker'
+    },
+    {
+        name: 'Princess Leia', 
+        role: 'General Princess', 
+        forcePoints: 100,
+        age: 40,
+        avatarImg: 'https://cdn.theatlantic.com/thumbor/Db7r0Y1f6nGfb00pBWfXia8LmAg=/199x0:1401x1202/500x500/media/img/mt/2014/04/leia/original.jpg',
+        routeName: 'princessleia'
+    }
+]
+
 app.get('/', (req, res) => {
     res.send('May the force be with you!')
+})
+
+app.get('/api/characters', (req,res) => {
+    res.json(characters)
 })
 
 app.listen(PORT, () => {
